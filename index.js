fd7f44732d92e52cccb257f6c8ff0d83b21fdec9
Matthew Brophy <MBrophy@Matthews-MacBook-Air.local>
2019-03-19 09:39:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
 /****************************** DON'T ALTER ******************************/
 function fadeOut(el) {
-  el.classList.add("fade-up-out")
-  setTimeout(() => {
-    el.style.opacity = 0
-    el.classList.remove("fade-up-out")
-    el.style.pointerEvents = "none"
-  }, FADEDURATION)
+	el.classList.add('fade-up-out');
+	setTimeout(() => {
+		el.style.opacity = 0;
+		el.classList.remove('fade-up-out');
+		el.style.pointerEvents = 'none';
+	}, FADEDURATION);
 }
 
 function fadeIn(el) {
-  el.classList.add("fade-down-in")
-  setTimeout(() => {
-    el.style.opacity = 1
-    el.classList.remove("fade-down-in")
-    el.style.pointerEvents = "auto"
-  }, FADEDURATION)
+	el.classList.add('fade-down-in');
+	setTimeout(() => {
+		el.style.opacity = 1;
+		el.classList.remove('fade-down-in');
+		el.style.pointerEvents = 'auto';
+	}, FADEDURATION);
 }
 
 function transitionPage(el, groupOut, groupIn) {
-  fadeAllOut(el, groupOut)
-  setTimeout(() => {
-    fadeAllIn(groupIn)
-  }, LONGESTPOSSIBLE)
+	fadeAllOut(el, groupOut);
+	setTimeout(() => {
+		fadeAllIn(groupIn);
+	}, LONGESTPOSSIBLE);
 }
 /*************************************************************************/
 
-function delayedFadeOut(div, range) {
-  // Your solution here
-  fadeOut(div)
+function delayedFadeOut(div, range, min = 0) {
+	setTimeout(() => {
+		fadeOut(div);
+	}, Math.random() * range + min);
 }
 
-function delayedFadeIn(div, range) {
-  // Your solution here
-  fadeIn(div)
+function delayedFadeIn(div, range, min = 0) {
+	setTimeout(() => {
+		fadeIn(div);
+	}, Math.random() * range + min);
 }
 
 function fadeAllOut(el, group) {
-  // Your solution here
-  group.forEach(div => {
-    delayedFadeOut(div)
-  })
+	group.forEach((div) => {
+		if (div !== el) delayedFadeOut(div, SHORTRANGE);
+	});
+	delayedFadeOut(el, LONGRANGE, MINDELAY);
 }
 
 function fadeAllIn(group) {
-  // Your solution here
-  group.forEach(div => {
-    delayedFadeIn(div)
-  })
+	group.forEach((div) => {
+		delayedFadeIn(div, SHORTRANGE);
+	});
+}
+
+function transitionPage(el, groupOut, groupIn) {
+	fadeAllOut(el, groupOut);
+	setTimeout(() => {
+		fadeAllIn(groupIn);
+	}, LONGESTPOSSIBLE);
 }
